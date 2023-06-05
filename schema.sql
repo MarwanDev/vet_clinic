c61a40a0e4d32c9500f67f79ea76703a082efdae
MarwanDev <marwaneg@aucegypt.edu>
2023-06-05 22:51:05 +0300

    Finalize animals table requirements

---
+++ schema.sql
@@ -0,0 +1,10 @@
+/* Database schema to keep the structure of entire database. */
+
+create table animals (
+    id int primary key GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250),
+    date_of_birth DATE,
+    escape_attempts INTEGER,
+    neutered BOOLEAN,
+    weight_kg DECIMAL
+);
