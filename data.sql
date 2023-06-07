a3f4420d1a8159a59b2edd29e1b208cd2f6f6f2d
MarwanDev <marwaneg@aucegypt.edu>
2023-06-07 18:13:40 +0300

    Finalize multiple tables

--- data.sql
+++ data.sql
@@ -15,3 +15,48 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 ('Boarmon', '2005-6-7', 7, true, 20.4),
 ('Blossom', '1998-10-13', 3, true, 17),
 ('Ditto', '2022-5-14', 4, true, 22);
+
+
+INSERT INTO owners (full_name, age) values
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob ', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+select * from animals
+
+
+INSERT INTO species (name) values
+('Pokemon'), ('Digimon');
+select * from species
+
+begin;
+update animals
+set species_id = 2
+where name like '%mon';
+update animals
+set species_id = 1
+where species_id is null;
+commit;
+select * from animals
+
+
+begin;
+update animals
+set owner_id = 1
+where name in ('Agumon');
+update animals
+set owner_id = 2
+where name in ('Gabumon', 'Pikachu');
+update animals
+set owner_id = 3
+where name in ('Devimon', 'Plantmon');
+update animals
+set owner_id = 4
+where name in ('Charmander', 'Squirtle', 'Blossom');
+update animals
+set owner_id = 5
+where name in ('Angemon', 'Boarmon');
+commit;
+select * from animals
